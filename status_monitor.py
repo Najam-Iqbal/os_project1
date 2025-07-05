a70bbe79da53907b6889fcf51a9ef602f90f0ac9
Najam-Iqbal <163505432+Najam-Iqbal@users.noreply.github.com>
2025-07-05 19:39:05 +0500

    Update status_monitor.py

--- status_monitor.py
+++ status_monitor.py
@@ -27,8 +27,18 @@ def show_power_status():
         elapsed = time.time() - st.session_state.power_checked_at
         if elapsed < 3:
             if "ON" in st.session_state.power_result:
+               if get_value("led/manualcontrol"): 
+                st.info(f"{st.session_state.power_result}_ Manual Control Till {get_value("fr_end/noted_time")}")
+               elif get_value(schedule_string) != "":
+                st.info(f"{st.session_state.power_result}_ Following Timetable")
+               else:
                 st.info(st.session_state.power_result)
             else:
-                st.warning(st.session_state.power_result)
+               if get_value("led/manualcontrol"): 
+                st.Warning(f"{st.session_state.power_result}_ Manual Control Till {get_value("fr_end/noted_time")}")
+               elif get_value(schedule_string) != "":
+                st.Warning(f"{st.session_state.power_result}_ Following Timetable")
+               else:
+                st.Warning(st.session_state.power_result)
         else:
             st.session_state.show_power = False  # Hide after 3 seconds
