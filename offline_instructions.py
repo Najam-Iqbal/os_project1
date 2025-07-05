3808e249d36bc6f4bc4541642f32d7eaca1cf192
Najam-Iqbal <163505432+Najam-Iqbal@users.noreply.github.com>
2025-07-05 18:37:46 +0500

    Update offline_instructions.py

--- offline_instructions.py
+++ offline_instructions.py
@@ -8,7 +8,7 @@ def run():
  1. **Be near the device** (within approximately **15 meters**).
  2. **Scan for available WiFi networks** on your phone or computer. You will see a network with the **same name as your device**.
  3. **Connect to that network** using your device's password.
- 4. Once connected, **[click here](http://192.168.4.1/)** to open the configuration page.
+ 4. Once connected, **[click here](http://192.168.4.1/) or type 192.168.4.1** in browser to open the configuration page.
  5. **Enter your WiFi credentials** (SSID and password) and submit.
  """)
 
