0b9acb0ad9631dc7b7a8b5f997f0a6244f63aa0a
Najam-Iqbal <163505432+Najam-Iqbal@users.noreply.github.com>
2025-07-09 13:32:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from firebase_utils import validate_login, get_power_status, check_wifi_status
+from firebase_utils import validate_login, get_power_status, check_wifi_status, update_value
 from status_monitor import show_power_status
 import importlib
 import time
